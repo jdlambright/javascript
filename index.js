5c287fb3ce8f68f5899247d272ae5099ee1f16db
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-08-09 23:04:44 -0400

    updated color

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-document.query
-document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
+
+
+document.getElementById("title").innerHTML = "Goodbye";
+document.getElementsByClassName("btn")[0].style.backgroundColor = "green";
+document.getElementsByTagName("li")[2].style.color = "pink";
+document.querySelector("li a").style.color = "red";
+document.querySelectorAll("#list .item")[1].style.color = "blue";
