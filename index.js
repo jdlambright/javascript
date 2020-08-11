1cd6958c72fb18b104ae012567556f64bf117e04
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-08-10 22:11:39 -0400

    changed color

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 document.getElementById("title").innerHTML = "Goodbye";
-document.getElementsByClassName("btn")[0].style.backgroundColor = "green";
+document.getElementsByClassName("btn")[0].style.backgroundColor = "yellow";
 document.getElementsByTagName("li")[2].style.color = "pink";
 document.querySelector("li a").style.color = "red";
 document.querySelectorAll("#list .item")[1].style.color = "blue";
