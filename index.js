f2d36a5a41107106f13de7a51c04f3b9a6c6b1a6
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-08-10 22:27:55 -0400

    added comments

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
-
+// these are examples for the .getElement way of selecting features
 document.getElementById("title").innerHTML = "Goodbye";
 document.getElementsByClassName("btn")[0].style.backgroundColor = "yellow";
 document.getElementsByTagName("li")[2].style.color = "pink";
+
+// this how to adjust styles with the .querySelector method
 document.querySelector("li a").style.color = "red";
 document.querySelectorAll("#list .item")[1].style.color = "blue";
+
+// this is how you affect an attribute first you have to get it then set it
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
